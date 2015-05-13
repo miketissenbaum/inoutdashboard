fcaeb6d73a78326ce450e4b21a5c2203553e9e44
Mike T <miketissenbaum@gmail.com>
2015-05-13 10:05:47 -0500

    added inoutdashboard.py

--- inoutdashboard.py
+++ inoutdashboard.py
@@ -13,48 +13,25 @@ p = 0
 while True:
     print "Checking " + time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
 
-    result = bluetooth.lookup_name('BC:F5:AC:5D:93:7C', timeout=5)
+    result = bluetooth.lookup_name('AD:35:AF:4D:83:7C', timeout=5)
     if (result != None):
         m = "In"
         print "Mike: in"
-        #return (m)
     else:
         m = "Out"
         print "Mike: out"
-        #return(m)
 
-    result = bluetooth.lookup_name('00:61:71:C1:1B:42', timeout=5)
+    result = bluetooth.lookup_name('04:6D:77:C0:1F:42', timeout=5)
     if (result != None):
         p = "In"
         print "Adam: in"
-        #return(p)
     else:
         p = "Out"
         print "Adam: out"
-        #return(p)
 
-    result = bluetooth.lookup_name('0C:14:20:0F:27:C3', timeout=5)
-    if (result != None):
-        c = "In"
-        print "Craiggers: in"
-        #return(c)
-    else:
-        c = "Out"
-        print "Craiggers: out"
-        #return(c)
-
-    result = bluetooth.lookup_name('B8:5A:73:DD:DF:72', timeout=5)
-    if (result != None):
-        l = "In"
-        print "Lauren: in"
-        #return(l)
-    else:
-        l = "Out"
-        print "Lauren: out"
-        #return(l)
 
     with open("text.json", "w") as file:
-        json.dump({'Mike':m, 'Adam':p, 'Craiggers':c, 'Lauren':l}, file, indent=4) 
+        json.dump({'Mike':m, 'Adam':p}, file, indent=4) 
     time.sleep(60)
 
 with open("test") as file:
